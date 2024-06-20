7709710ab4684aa43df7a0e8d694f6715e17f73e
kistenklaus <karlsasssie@gmail.com>
2024-06-21 00:37:58 +0200

    add print

--- src/handle_heartbeat.cpp
+++ src/handle_heartbeat.cpp
@@ -1,13 +1,14 @@
-
 #include "canzero/canzero.h"
-#include <cstdio>
+#include "print.h"
 #include <cinttypes>
 
 
 void canzero_can0_wdg_timeout(uint8_t node_id) {
+  debugPrintf("CAN0 Heartbeat Miss %u\n", node_id);
   canzero_set_error_heartbeat_miss(error_flag_ERROR);
 }
 
 void canzero_can1_wdg_timeout(uint8_t node_id) {
+  debugPrintf("CAN1 Heartbeat Miss %u\n", node_id);
   canzero_set_error_heartbeat_miss(error_flag_ERROR);
 }
